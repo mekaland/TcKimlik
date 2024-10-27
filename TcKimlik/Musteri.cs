768cbe29bf0cfd6a701f11a3a2a075462b90c5c4
Kadir Dagli <mehmetkadirdagli27@gmail.com>
2024-10-27 21:36:24 +0300

    Add project files.

---
+++ TcKimlik/Musteri.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcKimlik
+{
+    internal class Musteri
+    {
+        private string _tckimliknumarasi;
+
+        public string tckimliknumarasi
+        {
+            get { return _tckimliknumarasi; }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    bool bayrak = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool karakterKontrol = char.IsNumber(value[i]);
+                        if (!karakterKontrol)
+                        {
+                            bayrak = true;
+                            break;
+                        }
+                    }
+
+                    if (bayrak)
+                    {
+                        Console.WriteLine("TC kimlik numarasındaki değerler sayısal olmalıdır.");
+                    }
+                    else
+                    {
+                        _tckimliknumarasi = value; // Değişiklik burada
+                        Console.WriteLine("Tebrikler, işte TC kimlik numaranız: " + _tckimliknumarasi);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("TC kimlik numarası 11 haneli olmalıdır.");
+                }
+            }
+        }
+    }
+}
---
+++ TcKimlik/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcKimlik;
+
+namespace TcKimlik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            bool gecerliMi = false;
+
+            while (!gecerliMi)
+            {
+                Console.Write("TC Kimlik Numaranızı Giriniz: ");
+                string tc = Console.ReadLine();
+                m1.tckimliknumarasi = tc; // Kullanıcıdan alınan değer burada set ediliyor
+
+                // Eğer tckimliknumarasi setter'ında bir hata yoksa gecerliMi'yi true yapıyoruz
+                if (m1.tckimliknumarasi != null) // _tckimliknumarasi null değilse geçerli
+                {
+                    gecerliMi = true;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
